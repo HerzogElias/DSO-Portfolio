21e5072a3bce86823a0e6efcd33cdeeb371336ab
Elias Herzog <elias.herzog.02@gmail.com>
2025-10-22 13:28:04 +0200

    feat:added header component

---
+++ src/profile-components/HeaderComponent.tsx
@@ -0,0 +1,10 @@
+function Navbar(){
+  return (
+    <nav>
+       <a href="#about">About me </a>
+       <a href="#skills">My skills</a>
+       <a href="#projects">My projects</a>
+       <a href="#contact">Contact</a>
+    </nav>
+  );
+}
---
+++ src/profile-components/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderComponent from './HeaderComponent';
+
+function Render() {
+  return (
+    <div>
+      <HeaderComponent></HeaderComponent>
+    </div>
+  );
+}
+
+export default Render;
