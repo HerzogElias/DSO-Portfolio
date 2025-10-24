e08607d5becb7b66871435896d9772fb3ec7c147
Elias Herzog <elias.herzog.02@gmail.com>
2025-10-24 15:07:49 +0200

    reafactor:added index.html to pages/portfolio directory

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import HeaderNavbar from "./HeaderComponent/HeaderComponent";
-
-export default function Render(){
-  <HeaderNavbar></HeaderNavbar>
-}
---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import HeaderNavbar from "../../components/HeaderComponent/HeaderComponent";
+
+export default function Render(){
+  return <HeaderNavbar></HeaderNavbar>
+}
