88a7aae5bc455b53f2f08b2e63ce9fdf443b46f7
Elias Herzog <elias.herzog.02@gmail.com>
2025-10-25 13:53:19 +0200

    style:added style on header-component navbar

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "./HeaderComponent.css";
+import styles from "./HeaderComponent.module.css";
 
-export default function HeaderNavbar(){
+export default function HeaderNavbar() {
   return (
-    <nav>
-       <a href="#about">About me </a>
-       <a href="#skills">My skills</a>
-       <a href="#projects">My projects</a>
-       <a href="#contact">Contact</a>
-    </nav>
+    <div className={styles.header}>
+      <nav>
+        <a href="#about">About me</a>
+        <a href="#skills">My skills</a>
+        <a href="#projects">My projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </div>
   );
 }
