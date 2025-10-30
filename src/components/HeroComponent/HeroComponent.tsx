54d60b03e4315970a77ed30fb8bdd8fdfd432581
Elias Herzog <elias.herzog.02@gmail.com>
2025-10-30 14:18:45 +0100

    style:bug-no style on heroComponent Section center

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -4,20 +4,25 @@ import HeaderNavbar from "../../components/HeaderComponent/HeaderComponent";
 
 export default function HeroComponent() {
   return (
-    <section>
-       <HeaderNavbar></HeaderNavbar>
+    <section className={styles.section}>
+      <HeaderNavbar />
+      
       <div className={styles.hero}>
-        <div className="hero-myprofile">
-          <h3> Hey there I`am </h3>
-          <h1> Elias Herzog</h1>
-          <p> Hallo you! I am Elias Herzog, and I`am Fullstack Developer with Spezelasation in DevSecOps.</p>
-          <button>Contact me</button>
+        <div className={styles.heroContent}>
+          <div className={styles.heroMyprofile}>
+            <h3>Hey there I`m</h3>
+            <h1>Elias Herzog</h1>
+            <p>
+              Hallo you! I am Elias Herzog, and I`m a Fullstack Developer with
+              specialization in DevSecOps.
+            </p>
+            <button>Contact me</button>
+          </div>
+          <div>
+            <img src="/img/foto_elias.png" alt="Elias Herzog" />
+          </div>
         </div>
-      </div>
-      <div>
       </div>
     </section>
   );
 }
-
-
