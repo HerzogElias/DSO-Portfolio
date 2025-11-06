7d4a2577a8701743f107ffa94be4af1fe87f634c
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-06 13:24:07 +0100

    refact: addded structure for herocomponent and other content

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -6,22 +6,26 @@ import MySkills from "@site/src/components/MySkillsComponent/MySkillsComponent";
 export default function Render() {
   return (
     <section className="about-me">
-      <div className="content">
-        <HeroComponent></HeroComponent>
-         <MySkills 
-        skills={[
-          { label: "HTML", imagePath: "img/skills/HTML.png" },
-          { label: "CSS", imagePath: "img/skills/CSS.png" },
-          { label: "Docusaurus", imagePath: "img/skills/Docusaurus.png" },
-          { label: "Python", imagePath: "img/skills/Python.png" },
-          { label: "Shell scripting", imagePath: "img/skills/Terminal.png" },
-          { label: "YAML", imagePath: "img/skills/YAML.png" },
-          { label: "Container", imagePath: "img/skills/Docker.png" },
-          { label: "CI/CD with Github Actions", imagePath: "img/skills/Actions.png" },
-          { label: "IT Security", imagePath: "img/skills/Security.png" }
+      <div className="hero-section">
+        <div className="content">
+          <HeroComponent />
+        </div>
+      </div>
 
-        ]}
-      />
+      <div className="content">
+        <MySkills
+          skills={[
+            { label: "HTML", imagePath: "img/skills/HTML.png" },
+            { label: "CSS", imagePath: "img/skills/CSS.png" },
+            { label: "Docusaurus", imagePath: "img/skills/Docusaurus.png" },
+            { label: "Python", imagePath: "img/skills/Python.png" },
+            { label: "Shell scripting", imagePath: "img/skills/Terminal.png" },
+            { label: "YAML", imagePath: "img/skills/YAML.png" },
+            { label: "Container", imagePath: "img/skills/Docker.png" },
+            { label: "CI/CD with Github Actions", imagePath: "img/skills/Actions.png" },
+            { label: "IT Security", imagePath: "img/skills/Security.png" }
+          ]}
+        />
       </div>
     </section>
   );
