85232ad216218c1cd0053c9be7b9f3d4605b61e5
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-13 14:16:42 +0100

    feat:added Rounting on ImprintComponent to Home

--- src/pages/imprint/index.tsx
+++ src/pages/imprint/index.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "./index.css";
+import Link from '@docusaurus/Link';
+
 
 export default function ImprintPage() {
-  return (
-      <section id="imprint" className="imprint">
-        <div>
-          <p>
-            Elias Herzog <br />
-            Beispielstraße 123 <br />
-            12345 Musterstadt
-          </p>
-        </div>
-      </section>
-  );
+    return (
+        <section id="imprint" className="imprint">
+            <div className="imprint-card">
+                <h1 className="imprint-title">Impressum</h1>
+                <p className="imprint-text">
+                    Elias Herzog <br />
+                    Zum Leppetal 11 <br />
+                    51709 Marienheide
+                </p>
+
+
+                <p className="imprint-subtitle">Kontaktmöglichkeiten:</p>
+                <div className="contactContactData">
+                    <p className="imprint-text">Email: contact@elias-herzog.de</p>
+                    <p className="imprint-text">Telefon: +49 1575 3683264</p>
+                </div>
+            </div>
+
+
+            <div className="contact">
+                <span>© Elias Herzog</span>
+                <Link to="imprint">
+                    Legal Notice
+                </Link>
+
+                <Link to="/portfolio">
+                    Home
+                </Link>
+
+
+            </div>
+        </section>
+    );
 }
