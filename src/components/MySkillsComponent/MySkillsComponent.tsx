bf716fae9acddff833a296a77ef4f0a0b42b1f24
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-17 16:33:34 +0100

    style:refactore style on Project Componennt

--- src/components/MySkillsComponent/MySkillsComponent.tsx
+++ src/components/MySkillsComponent/MySkillsComponent.tsx
@@ -10,12 +10,13 @@ export interface ISkillsProps {
   skills: ISkillElement[];
 }
 
-export default function MySkills({skills}: ISkillsProps) {
+export default function MySkills({ skills }: ISkillsProps) {
   return (
     <section id="my-skills" className={styles.mySkills}>
       <div className={styles.innerContent}>
         <h2>MY SKILLS</h2>
         <div className={styles.skillContent}>
+
           {skills.map((skill) => (
             <div key={skill.label} className={styles.skillDetail}>
               <img
