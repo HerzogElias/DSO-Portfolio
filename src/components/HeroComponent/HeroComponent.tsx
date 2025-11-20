7d94f5133395aeae9d465a82177d78d03c4ff1aa
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-20 22:16:32 +0100

    refact:test added type on img on Hero Component

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -20,7 +20,7 @@ export default function HeroComponent() {
             </a>
           </div>
           <div>
-            <img src="img/foto_elias.png" alt="Elias Herzog" />
+            <img src="img/foto_elias.png" alt="Elias Herzog" typeof="img" loading="lazy" />
           </div>
         </div>
       </div>
