8cd97afe78bae5102c68322ab29891c20511cf07
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-21 13:45:06 +0100

    feat:added routing to project component

--- src/components/MyProjectComponent/MyProjectComponent.tsx
+++ src/components/MyProjectComponent/MyProjectComponent.tsx
@@ -21,7 +21,7 @@ export default function MyProjects({ projects, title }: IProjectsProps) {
     const activeProject = projects[activeIndex];
 
     return (
-        <section className={styles.projectsSection} id="s">
+        <section className={styles.projectsSection} id="projects">
             <h2 className={styles.title}>{title}</h2>
 
             <div className={styles.wrapper}>
