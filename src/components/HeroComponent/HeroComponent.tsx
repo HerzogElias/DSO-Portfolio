8e87b5f5517eb35a21d6928977166df9ec55cc24
Elias Herzog <elias.herzog.02@gmail.com>
2025-11-28 14:59:47 +0100

    fear:added responsiveness for until max width 320px

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -6,12 +6,14 @@ export default function HeroComponent() {
   return (
     <section className={styles.section} id="about">
       <HeaderNavbar />
+
+      {/* Desktop Version */}
       <div className={styles.hero}>
         <div className={styles.heroContent}>
           <div className={styles.heroMyprofile}>
             <h3>Hey there I`m</h3>
             <h1>Elias Herzog</h1>
-            <h2>DevSecOps Engeneer</h2>
+            <h2>DevSecOps Engineer</h2>
             <p>
               Hallo you! I am Elias Herzog, and I`m a Fullstack Developer with
               specialization in DevSecOps.
@@ -21,8 +23,33 @@ export default function HeroComponent() {
             </a>
           </div>
           <div>
-            <img src="img/foto_elias.png" alt="Elias Herzog" typeof="img"/>
+            <img src="img/foto_elias.png" alt="Elias Herzog" />
           </div>
+        </div>
+
+        {/* Mobile Version */}
+        <div className={styles.heroContentRespo}>
+          <div className={styles.heroMyprofile}>
+            <h3>Hey there I`m</h3>
+            <h1>Elias Herzog</h1>
+            <h2>DevSecOps Engineer</h2>
+
+
+            <div>
+              <img src="img/foto_elias.png" alt="Elias Herzog" />
+            </div>
+
+            <p>
+              Hallo you! I am Elias Herzog, and I`m a Fullstack Developer with
+              specialization in DevSecOps.
+            </p>
+
+            <a href="#contact">
+              <button className={styles.button}>Contact me</button>
+            </a>
+
+          </div>
+
         </div>
       </div>
     </section>
