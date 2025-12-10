015cccfa43c347b41db36df41fc5e452e424aac6
Elias Herzog <elias.herzog.02@gmail.com>
2025-12-10 15:46:35 +0100

    test config docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   favicon: 'img/foto_elias.png',
 
   // Set the production url of your site here
-  url: process.env.DEPLOYMENT_URL ?? "https://herzogelias.github.io",
+  url: process.env.DEPLOYMENT_URL ?? "https://spmse.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.BASE_URL ?? "/",
